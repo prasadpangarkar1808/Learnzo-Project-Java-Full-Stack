70af673f7ab1e0174a73c67f45bf6408292867b8
Prasad Pangarkar <prasadpangarkar01@gmail.com>
2025-09-13 21:56:26 +0530

    Add files via upload

---
+++ learnzo-backend/learnzo-backend/src/main/java/com/learnzo/LearnzoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learnzo.learnzo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnzoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnzoBackendApplication.class, args);
+	}
+
+}
---
+++ learnzo-backend/learnzo-backend/src/test/java/com/learnzo/learnzo_backend/LearnzoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnzo.learnzo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnzoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
